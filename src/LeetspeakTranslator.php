28cbf296a9247cf2646fe7d9b325ef0d2ca483c3
Jenna Maloney <jmalo@uw.edu>
2015-11-17 20:18:58 -0800

    make all 5 tests pass

--- src/LeetspeakTranslator.php
+++ src/LeetspeakTranslator.php
@@ -11,7 +11,6 @@
             $phrase = str_replace($originals, $replacements, $phrase);
 
             $phrase = explode(" ", $phrase);
-    //as of here, phrase has been translated for specs 1-4 and turned into an array, where each key has a string value where each string is a word from the phrase
 
             $re_phrased = array();
 
@@ -19,7 +18,7 @@
 
             foreach ($phrase as $word)
             {
-//foreach word/string in the phrase/array, we're going to split it into individual letters
+//foreach word/string in the phrase/array, we're going to split it into individual letters as we iterate through each word
                 $letters = str_split($word);
 //so, should i now do what i need to do to the letters, based on key placement (if key is 0, leave the s alone?)
 //i need to iterate through the array of an idividual word, rejoin letters into individual word, push that word into rephrase array, and continue through cycle, as i don't want to have all individual letters being pushed into one single array without spaces
@@ -36,10 +35,14 @@
                         array_push($re_phrased, $convert);
                     }
                 }
+    //at this point, i've done what i need to do to each individual letter of a word, so the next step is to piece the letters back into a single word
                 $the_converted = implode($re_phrased);
+    //take this array of individual letters and implode it into a single string
                 array_push($phrase_back, $the_converted);
-                $translation_is = implode(" ", $re_phrased);
+    //that string containing a single word, needs to be placed back into an array, which will contain the other words in the sentence, converted as strtings in the same way
             }
+    //done with what is to be done to each individual word, so i've closed the foreach loop here. it should repeat this again until each word is "translated", then moved onto the implode equations below
+                $translation_is = implode(" ", $phrase_back);
                 //
                 // $newt = implode(" ", $re_phrased);
                 // array_push($phrase_back, $newt);
